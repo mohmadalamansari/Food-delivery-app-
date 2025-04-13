8e1782d88a2c7624fea69a95bdc9024d56c0bf8b
Rehan Alam <alamrehan12345678@gmail.com>
2025-04-14 01:40:06 +0530

    intitally file uploaded

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Nav from './compnonets/Nav'
+const Home = () => {
+  return (
+    <div className='w-full min-h-[100vh] bg-slate-200'>
+      <Nav/>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/compnonets/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { MdFastfood } from "react-icons/md";
+const Nav = () => {
+  return (
+    <div className='w-full h-[100px] '>
+      <div className='w-[60px] h-[60px] bg-white flex justify-center items-center'>
+      <MdFastfood />
+      </div>
+      <form>
+        
+      </form>
+    </div>
+  )
+}
+
+export default Nav
